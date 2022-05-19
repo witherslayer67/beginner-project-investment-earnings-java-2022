30e2e3c0007b94f458ddf7136be6752d692d5484
Jimmy McCosker <witherslayer67@gmail.com>
2022-05-19 14:49:17 -0400

    done
    5/19/22

---
+++ Driver.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.text.*;
+
+
+public class Driver {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your buying price per share.");
+        double buyingPrice = scan.nextDouble();
+        int day = 1;
+        double closingPrice = 0.1;
+        DecimalFormat df = new DecimalFormat("0.00"); //this line means that i dont have a decimal like 0.0000, will never go above 2 decimal places
+
+
+        while (true){
+            System.out.println("Enter the closing price for day" + day + " (any negative value to leave:)" );
+            closingPrice = scan.nextDouble();
+            if (closingPrice < 0.0) break;
+            double earnings = closingPrice - buyingPrice;
+            if (earnings > 0.0) {
+                System.out.println("After day " + day + ", you earned " + df.format(earnings) + " per share" );
+            }
+            else if (earnings < 0.0) {
+                System.out.println("After day " + day + ", you lost " + df.format(-earnings) + " per share" );
+            }
+            else {
+                System.out.println("After day" + day + ", you have " + "no earnings per share");
+            }
+            day += 1;
+        }
+        scan.close();
+    }
+}
